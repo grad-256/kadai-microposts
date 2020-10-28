b137462d0e93feef90d40a414f5b97238b9e3835
mmiyaudi <techacademy0525@gmail.com>
2020-10-28 08:14:37 +0000

    favorite_button 認証修正

--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -1,9 +1,9 @@
-@if (Auth::user()->is_favorite($micropost->user_id))
-    {!! Form::open(['route' => ['favorites.unfavorite', $micropost->user_id], 'method' => 'delete']) !!}
+@if (Auth::user()->is_favorite($micropost->id))
+    {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
         {!! Form::submit('UnFavorite', ['class' => "btn btn-danger btn-block"]) !!}
     {!! Form::close() !!}
 @else
-    {!! Form::open(['route' => ['favorites.favorite', $micropost->user_id]]) !!}
+    {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
         {!! Form::submit('Favorite', ['class' => "btn btn-primary btn-block"]) !!}
     {!! Form::close() !!}
 @endif
